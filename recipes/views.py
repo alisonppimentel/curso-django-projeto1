784d532ac43260567af052f1f73532f9cd027336
Alison P Pimentel <pereirapimentel@gmail.com>
2023-03-04 18:00:16 -0300

    update

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context = { 
-        'name': 'Luiz Otávio',                 
+        'name': 'Luiz Otávio'                
     })
 
 
